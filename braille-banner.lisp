9cddd5f3b899870345220ea0bb81239bd1f58c59
g000001 <chiba.masaomi@gmail.com>
2011-12-26 13:07:47 +0900

    関数名の命名に混乱があるけど、もう良いや

--- braille-banner.lisp
+++ braille-banner.lisp
@@ -9,18 +9,34 @@
 (defvar *k14*
   (kl:read-file-to-strings #p"k14.bdf"))
 
-(defun flatten (list)
-  (etypecase list
-    (null '() )
-    (cons (let ((head (car list))
-                (tail (flatten (cdr list))) )
-            (etypecase head
-              (atom (cons head tail))
-              (cons (append (flatten head) tail)) )))))
+;;; borrowed by on lisp
+(defun group (source n)
+  "groups every n elements together into new sublists.
+   e.g. (group '(1 a 2 b) 2) -> ((1 a) (2 b))"
+  (when (zerop n) (error "zero length"))
+  (labels ((rec (source acc)
+	     (let ((rest (nthcdr n source)))
+	       (if (consp rest)
+		   (rec rest (cons (subseq source 0 n) acc))
+		   (nreverse (cons source acc))))))
+    (if source
+	(rec source nil)
+	nil)))
+
+;;; bits
+(defun bitsp (list)
+  (and (listp list)
+       ;; (= 4 (length list))
+       (every (lambda (x)
+                (typep x '(and (integer 0 3))))
+              list)))
+
+(deftype bits ()
+  '(satisfies bitsp))
 
 ;;; dots
 (defun dotsp (list)
-  (and (consp list)
+  (and (listp list)
        (every (lambda (x)
                 (typep x '(and (integer 0 8))))
               list)))
@@ -29,44 +45,44 @@
   '(satisfies dotsp))
 
 (defun bits-to-dots (bits)
-  (let ((i 0)
-        (ans '() ) )
-    (dolist (e bits)
-      (PUSH
-       (case i
-         (3 (case e
-              (0 0)
-              (1 8)
-              (2 7)
-              (3 (list 7 8)) ))
-         (otherwise
-          (ecase e
-            (0 0)
-            (1 (+ 3 (1+ i)))
-            (2 (1+ i))
-            (3 (list
-                (+ 3 (1+ i))
-                (1+ i) )))))
-       ANS )
-      (incf i) )
-    (flatten ans)))
+  (declare (type bits bits)
+           (values dots))
+  (let ((i -1))
+    (mapcan (lambda (e)
+              (case (incf i)
+                (3 (case e
+                     (0 (list 0))
+                     (1 (list 8))
+                     (2 (list 7))
+                     (3 (list 7 8)) ))
+                (otherwise
+                 (ecase e
+                   (0 (list 0))
+                   (1 (list (+ 3 (1+ i))))
+                   (2 (list (1+ i)))
+                   (3 (list (+ 3 (1+ i))
+                            (1+ i) ))))))
+            bits)))
 
 (test bits-to-dots
-  (is (equal (bits-to-dots '(0 3 2 1))
-             '(8 3 5 2 0))))
+  (is (null (set-difference (bits-to-dots '(0 3 2 1))
+                            '(8 3 5 2 0)))))
 
-(defun list-to-braille-name (list)
+(declaim (ftype (function (dots) character)
+                dots-to-braille-char))
+
+(defun dots-to-braille-char (dots)
   (let ((ans (map 'string
                   (lambda (e)
                     (character (princ-to-string e)))
-                  (sort (remove 0 list) #'<))))
+                  (sort (remove 0 dots) #'<))))
     (if (string= "" ans)
         #\BRAILLE_PATTERN_BLANK
         (name-char
          (format nil "BRAILLE_PATTERN_DOTS-~A" ans)))))
 
-(test list-to-braille-name
-  (is (equal (mapcar #'list-to-braille-name
+(test dots-to-braille-char
+  (is (equal (mapcar #'dots-to-braille-char
                      '((0 0 0 0) (0 0 0 0) (7 6 5 4 1) (0 0 5 2 0)
                (8 3 5 2 0) (7 0 5 2 0) (0 6 5 2 0) (0 0 0 0)))
              '(#\BRAILLE_PATTERN_BLANK #\BRAILLE_PATTERN_BLANK
@@ -74,14 +90,28 @@
                #\BRAILLE_PATTERN_DOTS-2358 #\BRAILLE_PATTERN_DOTS-257
                #\BRAILLE_PATTERN_DOTS-256 #\BRAILLE_PATTERN_BLANK))))
 
-(defun bits-to-braille-name (bits)
-  (mapcar (lambda (x)
-            (list-to-braille-name
-             (bits-to-dots x)))
-          bits))
+(declaim (ftype (function (bits) character)
+                bits-to-braille-char))
 
-(test bits-to-braille-name
-  (is (equal (mapcar #'bits-to-braille-name
+(defun bits-to-braille-char (bits)
+  (dots-to-braille-char (bits-to-dots bits)))
+
+(defun font-line-p (font-line)
+  (and (listp font-line)
+       (every (lambda (x) (typep x 'bits))
+              font-line)))
+
+(deftype font-line ()
+  `(satisfies font-line-p))
+
+(declaim (ftype (function (font-line) list)
+                font-line-to-letter-line-chars))
+
+(defun font-line-to-letter-line-chars (font-line)
+  (mapcar #'bits-to-braille-char font-line))
+
+(test font-line-to-letter-line-chars
+  (is (equal (mapcar #'font-line-to-letter-line-chars
                      '(((0 0 0 0) (0 0 0 0) (3 1 1 2) (0 3 0 0)
                         (0 3 2 1) (0 3 0 2) (0 3 1 0) (0 0 0 0))
                        ((0 0 0 0) (3 2 0 2) (3 0 1 0) (3 2 1 3)
@@ -108,24 +138,34 @@
                 #\BRAILLE_PATTERN_DOTS-25 #\BRAILLE_PATTERN_DOTS-5))
              )))
 
-(defun hexs-to-bit-list (hexs)
-  (let ((bs hexs)
-        (ans '() ))
-    (dolist (b bs)
+(defun font-data-to-font-lines (font-data)
+  (group font-data 4))
+
+(defun font-line-to-letter-line (font-line)
+  (let ((ans '() ) )
+    (dolist (b font-line)
       (let ((tem '() ))
         (dotimes (i 8)
           (push (ldb (byte 2 (* 2 i)) b)
-                tem))
+                tem ))
         (push (reverse tem)
-              ans)))
+              ans )))
     (apply #'mapcar #'list
-           (nreverse ans))))
+           (nreverse ans) )))
 
-(test hexs-to-bit-list
-  (is (equal (mapcar #'hexs-to-bit-list '((48 16336 4624 2336)
-                                          (16380 9608 21584 5064)
-                                          (8712 2016 14912 384)
-                                          (1760 30748) ))
+(test font-data-to-letter-bits-line
+  (is (equal (font-line-to-letter-line '(48 16336 4624 2336))
+             '((0 0 0 0) (0 0 0 0) (3 1 1 2) (0 3 0 0)
+               (0 3 2 1) (0 3 0 2) (0 3 1 0) (0 0 0 0)))))
+
+(defun font-data-to-letter-line (font-data)
+  (mapcar #'font-line-to-letter-line
+          (font-data-to-font-lines font-data)))
+
+(test font-data-to-letter-line
+  (is (equal (font-data-to-letter-line
+              '(48 16336 4624 2336 16380 9608 21584 5064
+                8712 2016 14912 384 1760 30748))
              '(((0 0 0 0) (0 0 0 0) (3 1 1 2) (0 3 0 0)
                 (0 3 2 1) (0 3 0 2) (0 3 1 0) (0 0 0 0))
                ((0 0 0 0) (3 2 0 2) (3 0 1 0) (3 2 1 3)
@@ -135,18 +175,17 @@
                ((0 0) (0 3) (2 1) (3 0)
                 (2 0) (1 2) (0 3) (0 1))))))
 
-(defun hexs-to-braille-string (hexs)
+(defun font-line-to-braille-letter-line (font-line)
   (COERCE (reverse
-           (bits-to-braille-name
-            (hexs-to-bit-list hexs)))
+           (font-line-to-letter-line-chars
+            (font-line-to-letter-line font-line)))
           'STRING))
 
-(test hexs-to-braille-string
-  (is (equal (mapcar #'hexs-to-braille-string
-                     '((48 16336 4624 2336)
-                       (16380 9608 21584 5064)
-                       (8712 2016 14912 384)
-                       (1760 30748)))
+(test font-line-to-braille-letter-line
+  (is (equal (mapcar #'font-line-to-braille-letter-line
+                     (font-data-to-font-lines
+                      '(48 16336 4624 2336 16380 9608 21584 5064
+                        8712 2016 14912 384 1760 30748)))
              '("⠀⠲⡒⢖⠒⡹⠀⠀" "⠠⢫⠹⣙⣫⠩⡋⠀" "⠀⠥⠔⢗⡲⠂⠁⠀" "⠐⠒⠊⠁⠉⠑⠒⠀"))))
 
 (defun euc-octets-to-jis-code (octets)
@@ -168,35 +207,35 @@
   (is (= (char-to-jis-code #\愛)
          12326)))
 
-(defun char-to-font (char &aux (k14 *k14*))
-  (let ((code (char-to-jis-code char)))
-    (subseq (member (format nil
-                            "ENCODING ~A"
-                            code)
-                    k14 :test #'string=)
-            5 19)))
+(defun char-to-font-data (char &aux (k14 *k14*))
+  (let ((data (subseq (member (format nil
+                                      "ENCODING ~A"
+                                      (char-to-jis-code char))
+                              k14 :test #'string=)
+                      5 19)))
+    (mapcar (lambda (x)
+              (parse-integer x :radix 16))
+            data)))
 
-(test char-to-font
-  (is (equal (char-to-font #\愛)
-             '("0030" "3FD0" "1210" "0920" "3FFC" "2588" "5450"
-               "13C8" "2208" "07E0" "3A40" "0180" "06E0" "781C"))))
+(test char-to-font-data
+  (is (equal (char-to-font-data #\愛)
+             '(48 16336 4624 2336 16380 9608 21584 5064
+               8712 2016 14912 384 1760 30748))))
 
-(defun char-to-braille (char)
-  (mapcar #'hexs-to-braille-string
-          (sl:group (mapcar (lambda (x)
-                              (parse-integer x :radix 16))
-                            (char-to-font char))
-                    4) ))
+(defun char-to-braille-letter (char)
+  (mapcar #'font-line-to-braille-letter-line
+          (font-data-to-font-lines
+           (char-to-font-data char))))
 
-(test char-to-braille
-  (is (equal (char-to-braille #\愛)
+(test char-to-braille-letter
+  (is (equal (char-to-braille-letter #\愛)
              '("⠀⠲⡒⢖⠒⡹⠀⠀" "⠠⢫⠹⣙⣫⠩⡋⠀" "⠀⠥⠔⢗⡲⠂⠁⠀" "⠐⠒⠊⠁⠉⠑⠒⠀"))))
 
 (defun string-to-braille (str)
   (fare-utils:join-strings
    (apply #'mapcar (lambda (&rest e)
                      (apply #'concatenate 'string e))
-          (map 'list #'char-to-braille str))
+          (map 'list #'char-to-braille-letter str))
    :separator #\Newline))
 
 (test string-to-braille
